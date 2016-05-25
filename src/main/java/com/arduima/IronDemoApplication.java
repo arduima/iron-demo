7603a2095094f601dbb5d21054a63d6058660240
Dimitri <dimitri.koshkin@gmail.com>
2016-05-24 21:55:27 -0400

    New branch

--- src/main/java/com/arduima/IronDemoApplication.java
+++ src/main/java/com/arduima/IronDemoApplication.java
@@ -13,6 +13,6 @@ public class IronDemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello Iron.io");
+		System.out.println("Hello Iron.io Staging");
 	}
 }
